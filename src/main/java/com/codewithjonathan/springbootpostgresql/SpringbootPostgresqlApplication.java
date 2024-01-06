c0e5ac9212e18e45c7d1dcfaea3ad61f97363c68
Jonathan Tong <jontong321@gmail.com>
2024-01-06 16:19:30 -0500

    Completed project

---
+++ src/main/java/com/codewithjonathan/springbootpostgresql/SpringbootPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.codewithjonathan.springbootpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresqlApplication.class, args);
+	}
+
+}
